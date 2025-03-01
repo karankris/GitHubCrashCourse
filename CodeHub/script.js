ff656299cd9d0253c60c22e4a36d11bbe7e07da4
Karan <karan@Mac.lan>
2025-03-01 21:11:43 +0530

    Karan commit

---
+++ CodeHub/script.js
@@ -0,0 +1,24 @@
+var BE  =  { varient: { petrol:20 , disel: 70, battery: 30 } } //server API DATA 
+function Vechile() {
+    const car = BE;
+    const bikes = { varient: { petrol: 90, disel: 270, battery: 130 } }
+    //100
+    console.log("Fuel intaken by cars last year 2024 car:", 'car:', car, 'bikes:', bikes);
+    function carbrand() {
+        //200
+        const toyoto = car.varient.petrol; // petrol
+        const audi = car.varient.disel;
+        const tata = car.varient.battery;
+        console.log('Cars Brand:', 'Toyoto shared:', toyoto, 'Audi shared:', audi, 'Tata shared', tata);
+    }
+    carbrand();
+    function bike() {
+        //200
+        const honda = bikes.varient.petrol; // petrol
+        const java = bikes.varient.disel;
+        const Enfield = bikes.varient.battery;
+        console.log('Bike Brand:', 'Honda shared:', honda, 'Java shared:', java, 'Enfield shared', Enfield);
+    }
+    bike();
+}
+Vehile()
